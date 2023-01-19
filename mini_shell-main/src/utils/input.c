121a06531846b2c6ace8d3d0834922a9c414c2d7
Andrea Recce <arecce@e3r4p5.42roma.it>
2023-01-19 19:11:23 +0100

    utils

--- mini_shell-main/src/utils/input.c
+++ mini_shell-main/src/utils/input.c
@@ -6,7 +6,7 @@
 /*   By: arecce <arecce@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:06:05 by arecce            #+#    #+#             */
-/*   Updated: 2023/01/17 19:37:39 by arecce           ###   ########.fr       */
+/*   Updated: 2023/01/18 18:53:40 by arecce           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	split_input(t_prompt *p)
 
 	i = 0;
 	k = 0;
-	if (str_isalpha(p))
+	if (str_isalpha(p->cmd))
 		p->cmds = ft_split(p->cmd, ' ');
 }
 
@@ -28,8 +28,8 @@ void	get_input(t_prompt *p)
 	if (ft_strlen(p->cmd) != 0)
 	{
 		add_history(p->cmd);
-		// split_input(p);
-		p->cmds = ft_split(p->cmd, ' ');
+		split_input(p);
+		// p->cmds = ft_split(p->cmd, ' ');
 		exect(p);
 	}
 }
--- mini_shell-main/src/utils/input_utils.c
+++ mini_shell-main/src/utils/input_utils.c
@@ -6,22 +6,47 @@
 /*   By: arecce <arecce@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 19:03:36 by arecce            #+#    #+#             */
-/*   Updated: 2023/01/17 19:33:08 by arecce           ###   ########.fr       */
+/*   Updated: 2023/01/19 18:46:48 by arecce           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	str_isalpha(t_prompt *p)
+int	str_isalpha(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (p->cmd[i])
+	while (str[i])
 	{
-		if (!ft_isalpha(p->cmd[i]) && p->cmd[i] != '32')
+		if (!ft_isalpha(str[i]) && str[i] != ' ')
 			return (0);
 		i++;
 	}
 	return (1);
+}
+
+int	count_quotes(char *str)
+{
+	int	i;
+	int	dbl;
+	int	sgl;
+
+	i = 0;
+	dbl = 0;
+	sgl = 0;
+	while (str[i])
+	{
+		if (str[i] == "\'")
+			sgl++;
+		else if (str[i] == "\"")
+			dbl++;
+		i++;
+	}
+	if ((dbl % 2 == 0 && sgl == 0) || (sgl % 2 == 0 && dbl == 0))
+		return (1); //ignora le virgolette
+	else if ((dbl % 2 == 0 && sgl == 1) || (sgl % 2 == 0 && dbl == 1))
+		return (2); //quote position
+	else if ((dbl == 1 && sgl == 0) || (dbl == 0 && sgl == 1))
+		return (3); //dbquote
 }
